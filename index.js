e0ab07003f1eebcbd84acb5a45e39e5fc4da3187
Luane Domingos dos Santos <123187344+LuaneDSantos@users.noreply.github.com>
2023-12-06 21:47:59 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+function calcularNivel(vitorias, derrotas) {
+    let saldoVitorias = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias > 20 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias > 50 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias > 80 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias > 90 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    return { saldoVitorias, nivel };
+}
+
+// Exemplo de uso:
+const vitoriasJogador = 75;
+const derrotasJogador = 20;
+
+const resultado = calcularNivel(vitoriasJogador, derrotasJogador);
+console.log(`O Herói tem um saldo de ${resultado.saldoVitorias} está no nível de ${resultado.nivel}`);
